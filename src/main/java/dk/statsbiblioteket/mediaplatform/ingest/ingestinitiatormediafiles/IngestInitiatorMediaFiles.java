922ae32bbb390b49d42537f6ffd512a5899c88f6
Henning Böttger <heb@statsbiblioteket.dk>
2012-05-09 09:43:22 +0200

    INFRA-1 Initial ingest initiator: Nu med Nexus

--- src/main/java/dk/statsbiblioteket/mediaplatform/ingest/ingestinitiatormediafiles/IngestInitiatorMediaFiles.java
+++ src/main/java/dk/statsbiblioteket/mediaplatform/ingest/ingestinitiatormediafiles/IngestInitiatorMediaFiles.java
@@ -1,5 +1,8 @@
 package dk.statsbiblioteket.mediaplatform.ingest.ingestinitiatormediafiles;
 
+import dk.statsbiblioteket.mediaplatform.ingest.model.persistence.ChannelArchiveRequestDAOIF;
+
 public class IngestInitiatorMediaFiles {
 
+	private ChannelArchiveRequestDAOIF channelArchiveRequestDAO;
 }
