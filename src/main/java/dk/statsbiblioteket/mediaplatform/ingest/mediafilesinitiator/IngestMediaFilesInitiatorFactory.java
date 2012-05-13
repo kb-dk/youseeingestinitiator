df15e8bd6c39c01016cb604786ea2bdb93d31536
Henning Böttger <heb@statsbiblioteket.dk>
2012-05-13 20:43:37 +0200

    INFRA-11 - Implement algorithm for initiating total list of files: Ready for review

--- src/main/java/dk/statsbiblioteket/mediaplatform/ingest/mediafilesinitiator/IngestMediaFilesInitiatorFactory.java
+++ src/main/java/dk/statsbiblioteket/mediaplatform/ingest/mediafilesinitiator/IngestMediaFilesInitiatorFactory.java
@@ -1,37 +1,36 @@
 package dk.statsbiblioteket.mediaplatform.ingest.mediafilesinitiator;
 
+import java.io.File;
 import java.io.OutputStream;
 import java.util.Properties;
 
+import dk.statsbiblioteket.mediaplatform.ingest.model.persistence.HibernateUtil;
+import dk.statsbiblioteket.mediaplatform.ingest.model.service.ChannelArchiveRequestService;
 import dk.statsbiblioteket.mediaplatform.ingest.model.service.ChannelArchiveRequestServiceIF;
+import dk.statsbiblioteket.mediaplatform.ingest.model.service.YouSeeChannelMappingService;
 import dk.statsbiblioteket.mediaplatform.ingest.model.service.YouSeeChannelMappingServiceIF;
 
 public class IngestMediaFilesInitiatorFactory {
 
-    //private static final String HIBERNATE_CONFIG_FILE_PATH_KEY = "hibernate.config.file.path";
+    private static final String HIBERNATE_CONFIG_FILE_PATH_KEY = "hibernate.config.file.path";
 
     /**
-     * Creates instance of IngestInitiatorMediaFiles with db access
+     * Creates instance of IngestInitiatorMediaFiles with db access and an output stream
+     * that is directed to stdout
      * 
      * @param properties
      * @return
-     * @throws MissingPropertyException If property is missing
      */
     public static IngestMediaFilesInitiator create(Properties properties) {
-        /*
         String hibernateConfigFilePath = properties.getProperty(HIBERNATE_CONFIG_FILE_PATH_KEY);
         if (hibernateConfigFilePath == null) {
-            throw new MissingPropertyException("No entry found for property with key: " + HIBERNATE_CONFIG_FILE_PATH_KEY);
+            throw new RuntimeException("No entry found for property with key: " + HIBERNATE_CONFIG_FILE_PATH_KEY);
         }
         File cfgFile = new File(hibernateConfigFilePath);
         HibernateUtil.initialiseFactory(cfgFile);
-        ChannelArchiveRequestDAOIF channelArchiveRequestDAO = new ChannelArchiveRequestDAO();
-        Logger log = Logger.getLogger(IngestInitiatorMediaFiles.class);
-        */
-        
         OutputStream outputStream = System.out;
-        ChannelArchiveRequestServiceIF channelArchiveRequestService = null;//new ChannelArchiveRequestService();
-        YouSeeChannelMappingServiceIF youSeeChannelMappingService = null;//new YouSeeChannelMappingService();
+        ChannelArchiveRequestServiceIF channelArchiveRequestService = new ChannelArchiveRequestService();
+        YouSeeChannelMappingServiceIF youSeeChannelMappingService = new YouSeeChannelMappingService();
         IngestMediaFilesInitiator ingestInitiatorMediaFiles = new IngestMediaFilesInitiator(properties, channelArchiveRequestService, youSeeChannelMappingService, outputStream);
         return ingestInitiatorMediaFiles;
     }
